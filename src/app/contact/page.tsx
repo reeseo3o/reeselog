439e60e3f570d0c2b55544aebb2b96d03c368350
notyaeji <notyaeji@gmail.com>
2023-07-03 23:42:45 +0900

    add contact page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,3 +1,32 @@
+import ContactForm from '@/components/ContactForm';
+import { AiFillGithub, AiFillLinkedin, AiFillYoutube } from 'react-icons/ai';
+
+const LINKS = [
+  { icon: <AiFillGithub />, url: '' },
+  { icon: <AiFillYoutube />, url: '' },
+  { icon: <AiFillLinkedin />, url: '' },
+];
+
 export default function ContactPage() {
-  return <div>page</div>;
+  return (
+    <section className="flex flex-col items-center">
+      <h2 className="text-3xl font-bold my-2">Contact Me</h2>
+      <p>info@gmail.com</p>
+      <ul className="flex gap-4 my-2">
+        {LINKS.map((link, index) => (
+          <a
+            key={index}
+            href={link.url}
+            target="_blank"
+            rel="noreferrer"
+            className="text-5xl hover:text-yellow-400"
+          >
+            {link.icon}
+          </a>
+        ))}
+      </ul>
+      <h2 className="text-3xl font-bold my-8">Or Send me an email</h2>
+      <ContactForm />
+    </section>
+  );
 }
